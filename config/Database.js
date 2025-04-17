92b335c793b7de7ecbf91371f576aff5feb169f1
Ardiyanto Putra <ardiyantop8@gmail.com>
2025-04-17 11:21:11 +0700

    update Database.js now

--- config/Database.js
+++ config/Database.js
@@ -1,11 +1,19 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('crud_db', 'root', 'root', {
-    host: '127.0.0.1',
+// const db = new Sequelize('crud_db', 'root', 'root', {
+//     host: '127.0.0.1',
+//     dialect: 'mysql',
+//     port: 8889,
+// }
+// );
+const db = new Sequelize({
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
-    port: 8889,
-}
-);
+})
 // const db = mysql.createConnection({
 //     host: process.env.DB_HOST,
 //     port: process.env.DB_PORT,
